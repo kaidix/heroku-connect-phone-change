3b9286396ca2e1c3b4ec3bfe6f2c11f9e66e4287
Kaidi <kaidi.xu@salesforce.com>
2016-06-04 16:03:33 -0700

    save

--- server.js
+++ server.js
@@ -10,29 +10,27 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/update', function(req, res) {
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+    pg.connect(process.env.DATABASE_URL, function(err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1, Email = $5 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
-            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.old_email.trim(), req.body.email.trim()],
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1, Email = $5 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)', [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.old_email.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
-                  function(err, result) {
-                    done();
-                    if (err) {
-                        res.status(400).json({error: err.message});
-                    }
-                    else {
-                        // this will still cause jquery to display 'Record updated!'
-                        // eventhough it was inserted
-                        res.json(result);
-                    }
-                  });
-                }
-                else {
+                    conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)', [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                        function(err, result) {
+                            done();
+                            if (err) {
+                                res.status(400).json({
+                                    error: err.message
+                                });
+                            } else {
+                                // this will still cause jquery to display 'Record updated!'
+                                // eventhough it was inserted
+                                res.json(result);
+                            }
+                        });
+                } else {
                     done();
                     res.json(result);
                 }
@@ -41,6 +39,29 @@ app.post('/update', function(req, res) {
     });
 });
 
-app.listen(app.get('port'), function () {
+app.post('/heartbeat', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            conn.query('INSERT INTO salesforce.heartbeat__c (device_id__c, heartbeat__c) VALUES ($1, $2)', [req.body.device_id.trim(), req.body.heartbeat.trim()],
+                function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({
+                            error: err.message
+                        });
+                    } else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                }
+            );
+        );
+    });
+});
+
+app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
